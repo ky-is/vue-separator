43806c59d6e61626d3d758872e9b9fd1c0faf824
Kyle Coburn <kylecoburn@gmail.com>
2018-09-18 15:15:25 -0700

    🐛🏗 es5

--- src/index.vue
+++ src/index.vue
@@ -10,18 +10,18 @@ export default {
 	},
 
 	render (createElement, context) {
-		let children = null
-		const slots = context.slots()
-		const props = context.props
-		const slotChildren = slots.default
+		var children = null
+		var slots = context.slots()
+		var props = context.props
+		var slotChildren = slots.default
 		if (slotChildren) {
-			const separator = slots.separator || props.separator
+			var separator = slots.separator || props.separator
 			if (!separator) {
 				children = slotChildren
 			} else {
 				children = []
-				const lastIndex = slotChildren.length - 1
-				for (let index = 0; index <= lastIndex; index += 1) {
+				var lastIndex = slotChildren.length - 1
+				for (var index = 0; index <= lastIndex; index += 1) {
 					children.push(slotChildren[index])
 					if (index < lastIndex) {
 						children.push(separator)
